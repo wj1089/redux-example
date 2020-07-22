8f97f0159021ffa37d9a93e9b97a67d72bda5efc
wj1089 <wj1089@naver.com>
2020-07-22 20:20:51 +0900

    20.07.22
    20:20

--- src/counter/CounterComponent.tsx
+++ src/counter/CounterComponent.tsx
@@ -1,18 +1,6 @@
 import React, {useState} from 'react';
 
 
-const INCREASE = 'counter/INCREASE'
-const DECREASE = 'counter/DECREASE'
-
-export const increase = () => ({type:INCREASE})
-export const decrease = () => ({type:DECREASE})
-
-const intialState = {
-    number: 0
-}
-function counter(state, action){}
-// -------------------------------------
-
 //프로퍼티로 number, onIncrease, onDecrease를 jsx에 전달한다.
 const CounterComponent = ({number, onIncrease, onDecrease}) => {
 //()안에 그냥 넣어주면 state를 준게 아니다. //그래서 밖에 { }을 둘러준다.
